d487165c9802af4696314be199a038d48e970f23
CarEnriquez <carlos-eg@outlook.com>
2016-02-13 15:07:25 -0600

    Update Reference.cpp

--- Reference.cpp
+++ Reference.cpp
@@ -1,1 +1,7 @@
-
+//Entrada y salida en C++
+//Entrada
+cin >> aqui va la variable o mensaje ;
+scanf(); //Se tiene que dar formato de salida y entrada
+//Salida
+cout << aqui va la variable o mensaje ;
+printf(); //Se tiene que dar formato de salida
