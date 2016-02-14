1c000606ea2dae90e2b3c33f3a5b3cb32f483968
CarEnriquez <carlos-eg@outlook.com>
2016-02-13 20:10:25 -0600

    Update Reference.cpp

--- Reference.cpp
+++ Reference.cpp
@@ -6,3 +6,16 @@ scanf(); //Se tiene que dar formato de salida y entrada
 cout << aqui va la variable o mensaje ;
 printf(); //Se tiene que dar formato de salida
 ---------------------Sección sentencia if ----------------------
+//La sentencia if, evalua la condición que esta entre parentesis
+//si se cumple, se realizan las instrucciones dentro de las llaves
+if (condición){
+  //Aquí deben de ir las instrucciones a realizar
+}
+----------------Sub-Sección if-else ---------------
+//En esta variación de if, se puede agregar una instrucción en caso que no se cumpla la condición del if
+//esto se realiza en la parte del else
+if(condición){
+  //Aquí deben de ir las instrucciones a realizar en caso que se cumpla la condición
+}else{
+  //Aquí deben de ir las instrucciones a realizar en caso que NO se cumpla la condición
+}
