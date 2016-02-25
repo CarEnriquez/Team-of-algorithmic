1f9f37eda5bcb20c8ff246934caadf19ac306708
CarEnriquez <carlos-eg@outlook.com>
2016-02-25 15:45:48 -0600

    Update Reference.cpp

--- Reference.cpp
+++ Reference.cpp
@@ -19,3 +19,47 @@ if(condición){
 }else{
   //Aquí deben de ir las instrucciones a realizar en caso que NO se cumpla la condición
 }
+----------------Termina Sub-Sección if else ---------------
+
+----------------Sub-Sección if anidados ---------------
+//En esta variación de if, se puede agregar un if dentro de otro if
+//Ejemplo 1
+if(condición){
+  //Aquí deben de ir las instrucciones a realizar en caso que se cumpla la condición o incluso otro if
+  if(condición){
+    //Aquí deben de ir las instrucciones a realizar en caso que se cumpla la condición o incluso otro if
+  }
+}else{
+  //Aquí deben de ir las instrucciones a realizar en caso que NO se cumpla la condición
+}
+//Ejemplo 2
+if(condición){
+  //Aquí deben de ir las instrucciones a realizar en caso que se cumpla la condición o incluso otro if
+}else{
+  //Aquí deben de ir las instrucciones a realizar en caso que NO se cumpla la condición
+    if(condición){
+    //Aquí deben de ir las instrucciones a realizar en caso que se cumpla la condición o incluso otro if
+  }
+}
+----------------Termina Sub-Sección if anidados ---------------
+
+Ejemplo de programa
+https://omegaup.com/arena/problem/CR-Comparaciones#problems
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin >> a >>b;
+    if(a>b){
+        cout <<"Es mayor";
+    }else{
+       if(b>a){
+        cout <<"Es menor";
+       }else{
+         cout << "Son iguales";3
+       }
+    }
+    return 0;
+}
+----------------Termina Sección if ---------------
